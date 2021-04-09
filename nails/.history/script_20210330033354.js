23ac9cb4da3a724f35a2277e73571e5ae9570987
Demchuk Tatyana <69cobak@mail.ru>
2021-04-09 21:57:07 +0300

    feat: remove .history

--- nails/.history/script_20210330033354.js
+++
@@ -1,9 +0,0 @@
-console.log(document.querySelector('#form'))
-const token='1774086628:AAHSkme8PnEpidfLpDZs2iXlcQI9o6IebZ4';
-const chat='-547999288'
-
-document.querySelector('#form').addEventListener('submit', (e) => {
-  e.preventDefault()
-  const txt = document.querySelector('textarea').value;
-  fetch(`https://api.telegram.org/bot${token}/sendMessage?chat_id=${chat}&parse_mode=html&text=${txt}`)
-})
